2fdfa571fd3bd42df11390b69af69826bdb534d6
saraelbana <matika20@gmail.com>
2024-01-03 13:57:24 +0200

    fixing switch cases syntax error  in opdracht2  in loops.js

--- loops.js
+++ loops.js
@@ -50,14 +50,23 @@ console.log("klaar!")
 // ==========================================
 for( let i= 0; i<10;i++){
     switch (i){
-        case 0,1,2:
+        case 0:
+        case 1:
+            case 2:
+
             console.log(i);
             break;
-        case 3,4,5:
+        case 3:
+        case 4:
+            case 5:
+
             console.log("> "+ i);
             break;
 
-        case 6,7,8,9:
+        case 6:
+        case 7:
+        case 8:
+            case 9:
             console.log(">> "+ i);
             break;
         default:
