a9cec83afe4f50e6cf65dfb3613daed6bc5136e7
saraelbana <matika20@gmail.com>
2024-01-03 13:38:33 +0200

    finishing opdracht 3  in loops-arrays-objects.js

--- loops-arrays-objects.js
+++ loops-arrays-objects.js
@@ -149,5 +149,29 @@ let students = [
 // ]
 // ==========================================
 for (let i =0; i<students.length; i++){
-
+    switch (students[i].zipCode){
+        case "3513":
+            students[i].neighborhood = "Pijlsweerd";
+            break;
+        case "3514":
+            students[i].neighborhood = "Vogelenbuurt";
+            break;
+        case "3512":
+                students[i].neighborhood = "Binnenstad";
+            break;
+        case "3531":
+            students[i].neighborhood = "Lombok";
+            break;
+        case"3572":
+            students[i].neighborhood = "Wittevrouwen";
+            break;
+        case "3581":
+            students[i].neighborhood = "Oudwijk";
+            break;
+        case "3583":
+            students[i].neighborhood = "Schildersbuurt";
+            break;
+        default:
+            break;
+    }
 }
